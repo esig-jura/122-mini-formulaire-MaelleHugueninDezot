2de2a42db04e67af13b1c1a3c7054aecfcc89881
Maëlle Huguenin-Dezot <maelle.huguenin@divcom.ch>
2024-04-17 15:42:56 +0200

    contrôle que les mdp sont identiques

--- js/main.js
+++ js/main.js
@@ -5,4 +5,31 @@
 
 'use strict';
 
-console.log('Test liaison JS');
+//Récupération du formulaire
+const formulaire = document.querySelector('form');
+const txtMDP = document.querySelector('#mdp');
+const txtMDPConfirm = document.querySelector('#mdpc');
+console.log(formulaire, txtMDP, txtMDPConfirm);
+
+//Ecouter l'envoi du formulaire
+formulaire.addEventListener('submit', function coucou (event) {
+    //Stop l'envoi du formulaire
+    event.preventDefault();
+
+    //Récupérer la valeur du mdp et de la confirmation
+    let mdp = txtMDP.value;
+    let mdpConfirm = txtMDPConfirm.value;
+
+    //Tester si les mdp sont identiques
+    if(mdp !== mdpConfirm){
+        alert('Les mots de passe sont différents !')
+
+    }
+    else{
+        alert('Compte créé avec succès !');
+
+        //Envoi du formulaire
+        formulaire.submit();
+    }
+
+});
